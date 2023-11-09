00f55008476f0bfc7b1511d8144a969dd8ba52f8
buwituze <b.rutagengw@alustudent.com>
2023-11-09 15:53:43 -0800

    correcting nbr3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    # Initialize an empty set to store common elements
-    common_set = set()
-    # Iterate through elements 
-    for elem in set_1:
-        # Check if the element is present 
-        if elem in set_2:
-            # If present, add it to the common set
-            common_set.add(elem)
-            return common_set
+    # Use the intersection operator '&' to find common elements
+    common_set = set_1 & set_2
+    return common_set
