66428e5225614546c7a87f6880b1164b30b4f997
buwituze <b.rutagengw@alustudent.com>
2023-11-09 12:36:12 -0800

    meeting pep validation for q6,8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     # Check if the sentence is empty
     if not sentence:
-        return (0, None)  # Return a tuple with length 0 and first character as None
-    else:
-        return (len(sentence), sentence[0])  # Return a tuple with length and first character
+        return (0, None)
+     else:
+        return (len(sentence), sentence[0]) 
