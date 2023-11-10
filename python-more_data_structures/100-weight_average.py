8e5f2b5179622cac6dda877f708b235b8bb89e79
buwituze <b.rutagengw@alustudent.com>
2023-11-10 08:11:47 -0800

    solved adtask3 and corrected pep errors for adtask1

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    # Check if the list is empty
+ # Check if the list is empty
     if not my_list:
         return 0
 
-    #variables to store the sum of prod& the sum of weights
+# variables to store the sum of prod& the sum of weights
     sum_products = 0
     sum_weights = 0
 
-    #go through the list of tuples
+# go through the list of tuples
     for score, weight in my_list:
-        # Calculate the prodof score& weight, add it to the sum
+# Calculate the prodof score& weight, add it to the sum
         sum_products += score * weight
-        # Add the weight to the sum of weights
+# Add the weight to the sum of weights
         sum_weights += weight
-
-    #calculate the weighted average
+# calculate the weighted average
     weighted_avg = sum_products / sum_weights
 
     return weighted_avg
---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+#create a list to store keys that need to be deleted
+    keys_to_delete = []
+
+#go through the dictionary items
+    for key, val in a_dictionary.items():
+#check if the value matches the specified value
+        if val == value:
+#if a match is found, add the key to the list of keys to delete
+            keys_to_delete.append(key)
+
+#delete the keys with the specified value
+    for key in keys_to_delete:
+        del a_dictionary[key]
+
+    return a_dictionary
