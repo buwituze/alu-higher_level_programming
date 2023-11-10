f926c866613a0dc1a79bb81a27956f76631554b7
buwituze <b.rutagengw@alustudent.com>
2023-11-09 17:37:25 -0800

    3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,12 +3,13 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Error: Division by zero")
+        print("Error: Division by zero is not allowed.")
         return None
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print("An error occurred:", e)
         return None
     else:
+        print("Inside result: {}".format(result))
         return result
     finally:
-        print("Inside result: {}".format(result))
+        print("Finally block: Division operation completed.")
