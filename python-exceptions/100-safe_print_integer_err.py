3815bbca9d01758877969cd98bf04f8582c0287a
buwituze <b.rutagengw@alustudent.com>
2023-11-09 19:15:16 -0800

    100-safe_print_integer_err.py

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import sys 
+
+import sys
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
@@ -8,3 +10,4 @@ def safe_print_integer_err(value):
         return False
     else:
         return True
+
