aa157fa355aeb39c8c87d6145b5ce3ffb0442b88
buwituze <b.rutagengw@alustudent.com>
2023-11-10 08:28:14 -0800

    solved adtask2&fixed pep errors for adtask1

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -3,19 +3,15 @@ def weight_average(my_list=[]):
     # check if the list is empty
     if not my_list:
         return 0
-    
     # initialize variables to store the sum of products and the sum of weights
     sum_products = 0
     sum_weights = 0
-    
-    # iterate through the list of tuples
+     # iterate through the list of tuples
     for score, weight in my_list:
         # calculate the product of score and weight and add it to the sum
         sum_products += score * weight
         # add the weight to the sum of weights
         sum_weights += weight
-    
     # calculate the weighted average
     weighted_avg = sum_products / sum_weights
-    
     return weighted_avg
