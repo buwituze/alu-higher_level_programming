afe26e429592174ef55d141ed8e46b10a2e5b2c0
buwituze <b.rutagengw@alustudent.com>
2023-11-23 11:17:47 -0800

    task2 and task3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Exploring JSON object representation."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON rep of an object."""
+    return json.dumps(my_obj)
