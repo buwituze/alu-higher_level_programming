5484f1ed0cc31c5df52eab2cfcc8443307eb1a51
buwituze <b.rutagengw@alustudent.com>
2023-12-01 10:32:55 -0800

    tasko

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status """
-if __name__ == '__main__':
-    import urllib.request
+"""fetches https://intranet.hbtn.io/status"""
+import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == '__main__':
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t-".format(
+              type(html), html), "utf8 content:", html.decode("utf-8"))
