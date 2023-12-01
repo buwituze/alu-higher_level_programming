ad234df3d8281ded908b3270272ee74127022e1f
buwituze <b.rutagengw@alustudent.com>
2023-12-01 10:14:55 -0800

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ import urllib.request
 
 
 if __name__ == '__main__':
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
