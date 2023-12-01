c2f6f249ad31961a8dc5c07ba5e9f2093941a81f
buwituze <b.rutagengw@alustudent.com>
2023-12-01 11:07:16 -0800

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """
-Fetches https://intranet.hbtn.io/status
-use the package urllib
- body of the response must be displayed in tabulation before -
+This module fetches the status from a url.
 """
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+from urllib.request import urlopen, Request
 
-print('Body response:\n\t- type: {}'.format(type(html)))
-print('\t- content: {}'.format(html))
-print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    req = Request(url)
+
+    with urlopen(req) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
