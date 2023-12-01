ba1428226703fc4b19db499112f4e69bdaa174ef
buwituze <b.rutagengw@alustudent.com>
2023-11-30 22:54:38 -0800

    correcting adtask1***

--- python-network_0/100-status_code.sh
+++ python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
-#!/bin/bash 
-# Bashscript that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -o /dev/null -w "Status code: %{http_code}\n" "$1"
+#!/bin/bash
+# Sends a GET request to a given URL and display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
