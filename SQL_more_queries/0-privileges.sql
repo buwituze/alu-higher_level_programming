e689b857cad61172820736c223d81b3d67c77662
buwituze <b.rutagengw@alustudent.com>
2023-11-30 21:22:50 -0800

    correcting task 0,1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- All privileges of the users user_0d_1 and user_0d_2
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
+-- Localhost
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
