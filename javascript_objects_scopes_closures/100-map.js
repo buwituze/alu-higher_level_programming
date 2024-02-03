066ea3b6dcd3bcc1d58f135a48765f6097686f09
buwituze <b.rutagengw@alustudent.com>
2024-02-03 13:19:58 -0800

    debug

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const data = require('./100-data').list;
 
-const arr = data.map(x => x * data.indexOf(x));
-console.log(data);
-console.log(arr);
+const { list } = require('./100-data');
+
+console.log(list);
+console.log(list.map((element, idx) => element * idx));
