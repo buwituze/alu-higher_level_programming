4dd53cc50a7496c22cc31f365290fa59e5a07858
buwituze <b.rutagengw@alustudent.com>
2024-02-03 10:18:05 -0800

    debugging

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 0){
-console.log('No argument');}
-else if (process.argv.length === 1){
-console.log('Argument found');}
-else {
-console.log('Arguments found');}
+if (process.argv.length === 1) {
+  console.log('No argument');
+} else if (process.argv.length === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
