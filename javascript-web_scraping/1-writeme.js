e68ef2bb5f07c230cb92d31df34042fc1d6d872a
Benitha06 <benithatuze15@gmail.com>
2024-02-11 00:42:06 +0200

    debugging task1

--- javascript-web_scraping/1-writeme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, err => {
-  if (err) console.log(err);
-});
