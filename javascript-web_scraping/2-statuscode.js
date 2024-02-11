a5d49fdad83b852739724780438411373dd42bc9
buwituze <b.rutagengw@alustudent>
2024-02-11 14:05:59 +0200

    debug

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,4 +1,5 @@
-t request = require('request');
+#!/usr/bin/node
+const request = require('request');
 request(process.argv[2], function (err, response) {
 	  if (err) { console.error(err); }
 	  console.log('code:', response && response.statusCode);
