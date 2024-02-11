eb5c9c674b43c0b79c38c54bdbc422172e1100a1
buwituze <b.rutagengw@alustudent>
2024-02-11 13:27:44 +0200

    executing web-scraping

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,4 @@
+t fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
+	  if (err) { console.log(err); }
+});
---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,5 @@
+t request = require('request');
+request(process.argv[2], function (err, response) {
+	  if (err) { console.error(err); }
+	  console.log('code:', response && response.statusCode);
+});
