46bb02d5fb2d2f934802608988f6f9d6866b03de
buwituze <b.rutagengw@alustudent>
2024-02-11 13:53:56 +0200

    debug

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-const data = process.argv[3];
-
-fs.writeFile(filePath, data, (err) => {
-	  if (err) {
-		      console.log(err);
-		    } else {
-			        fs.readFileSync(filePath, 'utf8');
-			      }
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
+	  if (err) { console.log(err); }
 });
