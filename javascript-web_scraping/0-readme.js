95cb39fc362ff6249f375af17172b0fb3f2caa98
buwituze <b.rutagengw@alustudent>
2024-02-11 13:45:08 +0200

    validation

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-const args = process.argv;
-const fileA = args[2];
-
 const fs = require('fs');
 
-try {
-	  console.log(fs.readFileSync(fileA, 'utf8'));
-} catch (err) {
-	  /* errors */
-	  console.log(err);
-}
+fs.readFile(process.argv[2], (err, contents) => {
+	  err ? console.log(err) : console.log(contents.toString());
+});
