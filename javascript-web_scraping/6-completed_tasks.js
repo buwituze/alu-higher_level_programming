688d54a9e2079cab54e57e418f5a25ba1e390977
buwituze <b.rutagengw@alustudent>
2024-02-11 14:42:43 +0200

    validating

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,15 +1,13 @@
-t request = require('request');
+#!/usr/bin/node
+const request = require('request');
 const url = process.argv.slice(2)[0];
-
 request(url, (error, response, body) => {
 	  if (error) {
 		      console.log(error);
 		      return;
 		    }
-
 	  const todos = JSON.parse(body);
 	  const taskCompletedByUser = {};
-
 	  todos.forEach(todo => {
 		      if (todo.completed) {
 			            if (taskCompletedByUser[todo.userId] === undefined) {
